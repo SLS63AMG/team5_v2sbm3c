9905b9310210577251cd4efc241f1ab274a7ba1e
end599 <wogns00814@naver.com>
2025-01-06 18:20:28 +0900

    25/01/06

---
+++ src/main/java/dev/mvc/inquiry/InquiryVO.java
@@ -0,0 +1,56 @@
+package dev.mvc.inquiry;
+
+import org.springframework.web.multipart.MultipartFile;
+
+//CREATE TABLE inquiry (
+//    inquiryno NUMBER(10) NOT NULL,
+//    title VARCHAR2(500) NOT NULL,
+//    content CLOB NOT NULL,
+//    rdate DATE DEFAULT SYSDATE,
+//    state NUMBER(4) DEFAULT 1 NOT NULL,
+//    filename VARCHAR2(500),
+//    memberno NUMBER(10) NOT NULL,
+//    CONSTRAINT PK_inquiry_inquiryno PRIMARY KEY (inquiryno),
+//    CONSTRAINT FK_inquiry_memberno FOREIGN KEY (memberno) 
+//        REFERENCES member(memberno) 
+//        ON DELETE CASCADE
+//);
+
+public class InquiryVO {
+  /** 문의 사항 번호 */
+  private int inquiryno;
+  
+  /** 제목 */
+  private String title;
+  
+  /** 내용 */
+  private String content;
+  
+  /** 문의 사항 신청일 */
+  private String rdate;
+  
+  /** 문의 사항 처리 상태 */
+  private int state;
+  
+  /** 파일 이름 */
+  private String filename;
+  
+  /** 회원 번호 */
+  private int memberno;
+
+  /** ---------------------------------------------- */
+
+  /** 회원 이름 */
+  private String name;
+  
+  /** 파일 이름 */
+  private MultipartFile file1MF;
+
+
+
+
+
+
+
+
+}
