9714932eed899634dc37a6041f1165d0b776bedc
end599 <wogns00814@naver.com>
2025-01-14 13:16:02 +0900

    25/01/14

---
+++ src/main/java/dev/mvc/rating/RatingDAOInter.java
@@ -0,0 +1,5 @@
+package dev.mvc.rating;
+
+public interface RatingDAOInter {
+
+}
---
+++ src/main/java/dev/mvc/rating/RatingVO.java
@@ -0,0 +1,16 @@
+package dev.mvc.rating;
+
+public class RatingVO {
+  
+  /** 회원 번호 */
+  private int memberno;
+  
+  /** 회원 번호 */
+  private int storeno;
+  
+  /** 평점 */
+  private double rating;
+  
+  // 추가 항목 ----------------------------------
+  // 조인 항목 ----------------------------------
+}
